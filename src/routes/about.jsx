965ad73646d556b6ded8797ecc35f10eb84b4bce
jettison200 <jettison200@gmail.com>
2022-11-20 19:21:54 -0500

    feat: workflow test

--- src/routes/about.jsx
+++ src/routes/about.jsx
@@ -1,8 +1,8 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const about = () => {
   return (
-    <div className="app">
+    <div className='app'>
         <h1>About</h1>
                <Link to="/contact">Contact</Link>
                                                        <div className="btn">
